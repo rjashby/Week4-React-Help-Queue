ca9b6181b4132df51232f5561b2db5ec7fcba132
Jase Seeley <jcseeley31590@yahoo.com>
2022-04-04 13:15:37 -0700

    code through page 11 of React Fundamentals.

---
+++ src/components/App.js
@@ -0,0 +1,42 @@
+import React from "react";
+import Header from "./Header";
+import TicketList from "./TicketList";
+
+function App(){
+  const name = "Thato";
+  const name2 = "Haley";
+  return (
+    <React.Fragment>
+      <Header />
+      <TicketList />
+    </React.Fragment>
+  );
+}
+
+export default App;
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
