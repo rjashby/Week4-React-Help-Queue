8077c92bd384fc48d03d137147c6f07c4d0b846a
Jase Seeley <jcseeley31590@yahoo.com>
2022-04-18 14:51:50 -0700

    code through page 24 of React Fundamentals.

--- src/components/App.js
+++ src/components/App.js
@@ -1,45 +1,14 @@
 import React from "react";
 import Header from "./Header";
-import TicketList from "./TicketList";
+import TicketControl from "./TicketControl";
 
 function App(){
-  const name = "Thato";
-  const name2 = "Haley";
   return (
     <React.Fragment>
       <Header />
-      <TicketList />
+      <TicketControl />
     </React.Fragment>
   );
 }
 
 export default App;
-
-{/* This is a JSX comment. */}
-// Think about curly braces as a way to escape JSX back and return to vanilla JS.
-
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
