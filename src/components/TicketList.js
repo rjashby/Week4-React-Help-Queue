abde5cceedda59faf051c69cc2ba0872871b5400
Jase Seeley <jcseeley31590@yahoo.com>
2022-04-28 13:36:58 -0700

    Add code from page 9 of React with NoSQL.

--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -1,14 +1,23 @@
 import React from "react";
 import Ticket from "./Ticket";
 import PropTypes from "prop-types";
+import { useSelector } from 'react-redux';
+import { useFirestoreConnect, isLoaded, isEmpty } from 'react-redux-firebase';
 
 function TicketList(props){
   
-  return (
-    <React.Fragment>
-      <div className="grid grid-cols-5">
-        {Object.values(props.ticketList).map((ticket) =>
-          <Ticket 
+  useFirestoreConnect([
+    { collection: 'tickets' }
+  ]);
+
+  const tickets = useSelector(state => state.firestore.ordered.tickets);
+
+  if(isLoaded(tickets)) {
+    return (
+      <React.Fragment>
+        <div className="grid grid-cols-5">
+          {tickets.map((ticket) => {
+            return <Ticket 
             whenTicketClicked = { props.onTicketSelection }
             names={ticket.names}
             location={ticket.location}
@@ -16,14 +25,20 @@ function TicketList(props){
             formattedWaitTime={ticket.formattedWaitTime}
             id={ticket.id}
             key={ticket.id}/>
-        )}
-      </div>
-    </React.Fragment>
-  );
+        })}
+        </div>
+      </React.Fragment>
+    );
+  } else {
+    return (
+      <React.Fragment>
+        <h3>Loading...</h3>
+      </React.Fragment>
+    )
+  }
 }
 
 TicketList.propTypes = {
-  ticketList: PropTypes.object,
   onTicketSelection: PropTypes.func
 };
 
